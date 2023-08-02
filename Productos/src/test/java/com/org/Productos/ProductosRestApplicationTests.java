4e8b247640a596074580503218bc34d4562a2b57
Joel L.A <joel_la@outlook.es>
2023-08-02 10:35:49 -0500

    Creacion de proyecto

---
+++ Productos/src/test/java/com/org/Productos/ProductosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.Productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
